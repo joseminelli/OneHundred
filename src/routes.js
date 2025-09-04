014c6bd5ab010710151ce77ee44e8202680e5af9
José Minelli <zecarmineli@gmail.com>
2025-09-03 21:43:08 -0300

    feat: add GameDetailsPage component and update route import

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Index from '@/pages/index.vue';
 import NotFound from '@/pages/not-found.vue';
-import GameDetailsPage from '@/pages/game/GameDetailsPage.vue';
+import GameDetailsPage from '@/pages/GameDetailsPage.vue';
 const routes = [
   {
     path: '/',
